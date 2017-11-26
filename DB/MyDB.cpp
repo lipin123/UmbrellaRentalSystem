51851bf39cf63e5669ed3b1276307e4cae83392b
JaeMin Shin <alex9395@naver.com>
2017-11-26 11:55:23 +0900

    db update

--- DB/MyDB.cpp
+++ DB/MyDB.cpp
@@ -1,8 +1,10 @@
 #include<iostream>  
 #include<cstdlib>
+#include <string>
 #include "MyDB.h"
 
 using namespace std;
+
 
 MyDB::MyDB()
 {
@@ -69,10 +71,560 @@ vector<string> MyDB::NextRow() //Extract the next line of data
 
 }
 
+
+
 void MyDB::FreeResult() //Release the memory occupied by the result set
 {
 	if (result != NULL)
 		mysql_free_result(result);
 }
+
+vector<rentalSpot> MyDB::SearchSpot(double lat, double lng, int range)
+{
+	
+		vector<rentalSpot> re;
+		int res = 0, resTableRow = 0, resTableCol = 0;
+		string reqStr;
+
+		reqStr = "call dist(" + to_string(lat) + "," + to_string(lng) + "," + to_string(range) + ");";
+		res = mysql_query(connection, reqStr.c_str());
+		if (res)
+		{
+			cout << "Query Error:" << mysql_error(connection);
+			return re;
+		}
+		result = mysql_store_result(connection);
+		resTableCol = mysql_num_fields(result);
+		resTableRow = mysql_num_rows(result);
+
+
+		if (result)
+		{
+			
+			for (int p = 0; p < resTableRow; p++)
+			{
+				if (sql_row = mysql_fetch_row(result))
+				{
+					rentalSpot tem;
+					vector<string> temStrVec;
+					tem.rsID = atoi(sql_row[0]); //雨伞id
+
+					//储存雨伞的编号
+					SplitString(sql_row[1], temStrVec, ",");
+					for (int i = 0; i < temStrVec.size(); i++)
+						tem.umbrellaID.push_back(atoi(temStrVec[i].c_str()));
+
+					//将vacancy列中的数据分割成行和列,并储存。
+					vector <string>().swap(temStrVec);
+					SplitString(sql_row[2], temStrVec, "x");
+					tem.structure.row = atoi(temStrVec[0].c_str()); //租伞机器插槽的行
+					tem.structure.col = atoi(temStrVec[1].c_str()); //租伞机器插槽的列
+
+					string temvac = sql_row[3]; //保存插槽状态信息(是否可用)
+					tem.vacancy.reserve(tem.vacancy.size() + temvac.length());
+					tem.vacancy.insert(tem.vacancy.end(), temvac.c_str(), temvac.c_str() + temvac.length());
+
+					tem.lat = atof(sql_row[4]); //保存纬度
+					tem.lng = atof(sql_row[5]);	 //保存经度
+					re.push_back(tem);
+				}
+				else
+				{
+					break;
+				}
+			}
+
+		}
+		return re;
+
+	
+}
+
+vector<user> MyDB::UniSearch(userCol col, string val)
+{
+	int res = 0, resTableRow = 0;
+	vector<user> re;
+	string inqStr = "select * from user where ";
+	
+	switch (col)
+	{
+	case user_id:
+		inqStr += "user_id=";
+		break;
+	case user_acc:
+		inqStr += "user_acc=";
+		break;
+	case user_pw:
+		inqStr += "user_pw=";
+		break;
+	case umbrella:
+		inqStr += "umbrella=";
+		break;
+	case hash_code:
+		inqStr += "hash_code=";
+		break;
+	default:
+		break;
+	}
+	inqStr += val;
+
+	res = mysql_query(connection, inqStr.c_str());
+	if (res)
+	{
+		cout << "Query Error:" << mysql_error(connection);
+		return re;
+	}
+	result = mysql_store_result(connection);
+	resTableRow = mysql_num_rows(result);
+
+
+	if (result)
+	{
+		for (int p = 0; p < resTableRow; p++)
+		{
+			if (sql_row = mysql_fetch_row(result))
+			{
+				user tem;
+				tem.userID= atoi(sql_row[0]); //雨伞ID
+				tem.userAcc = sql_row[1];
+				tem.userPW = sql_row[2];
+				tem.umbrella = atoi(sql_row[3]);
+				tem.hashCode = sql_row[4];
+				re.push_back(tem);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return re;
+}
+
+vector<umbrellas> MyDB::UniSearch(umbrellaCol col, string val)
+{
+
+	int res = 0, resTableRow = 0;
+	vector<umbrellas> re;
+	string inqStr = "select * from umbrella where ";
+
+	switch (col)
+	{
+
+	case umbrella_id:
+		inqStr += "umbrella_id=";
+		break;
+	case umbrella_status:
+		inqStr += "umbrella_status=";
+		break;
+	case borrower_id:
+		inqStr += "borrower_id=";
+		break;
+	case slot_label:
+		inqStr += "slot_label=";
+		break;
+	default:
+		break;
+	}
+	inqStr += val;
+
+	res = mysql_query(connection, inqStr.c_str());
+	if (res)
+	{
+		cout << "Query Error:" << mysql_error(connection);
+		return re;
+	}
+	result = mysql_store_result(connection);
+	resTableRow = mysql_num_rows(result);
+
+	if (result)
+	{
+		for (int p = 0; p < resTableRow; p++)
+		{
+			if (sql_row = mysql_fetch_row(result))
+			{
+				umbrellas tem;
+				tem.umbrellaID = atoi(sql_row[0]); //雨伞ID
+				tem.umbrellaStatus = atoi( sql_row[1]);
+				tem.locationID = atoi(sql_row[2]);
+				tem.slotLabel = atoi(sql_row[3]);
+				re.push_back(tem);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return re;
+}
+
+vector<rentalSpot> MyDB::UniSearch(rentalSpotCol col, string val)
+{
+	int res = 0, resTableRow = 0;
+	vector<rentalSpot> re;
+	string inqStr = "select * from rental_spot where ";
+
+	switch (col)
+	{
+	case rs_id:
+		inqStr = inqStr+"rs_id="+val;
+		break;
+	case umbrellas_id:
+		inqStr = inqStr + "umbrellas_id='" + val + "';";
+		break;
+	case structure:
+		inqStr = inqStr + "structure='" + val + "';";
+		break;
+	case vacancy:
+		inqStr = inqStr+ "vacancy='" + val + "';";
+		break;
+	case latitude:
+		inqStr = inqStr + "latitude=" + val;
+		break;
+	case longitude:
+		inqStr = inqStr + "longitude=" + val;
+		break;
+	default:
+		break;
+	}
+	
+	res = mysql_query(connection, inqStr.c_str());
+	if (res)
+	{
+		cout << "Query Error:" << mysql_error(connection);
+		return re;
+	}
+	result = mysql_store_result(connection);
+	resTableRow = mysql_num_rows(result);
+
+	if (result)
+	{
+		for (int p = 0; p < resTableRow; p++)
+		{
+			if (sql_row = mysql_fetch_row(result))
+			{
+				rentalSpot tem;
+				vector<string> temStrVec;
+				tem.rsID = atoi(sql_row[0]); //雨伞id
+
+											 //储存雨伞的编号
+				SplitString(sql_row[1], temStrVec, ",");
+				for (int i = 0; i < temStrVec.size(); i++)
+					tem.umbrellaID.push_back(atoi(temStrVec[i].c_str()));
+
+				//将vacancy列中的数据分割成行和列,并储存。
+				vector <string>().swap(temStrVec);
+				SplitString(sql_row[2], temStrVec, "x");
+				tem.structure.row = atoi(temStrVec[0].c_str()); //租伞机器插槽的行
+				tem.structure.col = atoi(temStrVec[1].c_str()); //租伞机器插槽的列
+
+				string temvac = sql_row[3]; //保存插槽状态信息(是否可用)
+				tem.vacancy.reserve(tem.vacancy.size() + temvac.length());
+				tem.vacancy.insert(tem.vacancy.end(), temvac.c_str(), temvac.c_str() + temvac.length());
+
+				tem.lat = atof(sql_row[4]); //保存纬度
+				tem.lng = atof(sql_row[5]);	 //保存经度
+				re.push_back(tem);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return re;
+}
+
+bool MyDB::UniInsert(user row)
+{
+	string inqStr = "call Uni_insert('user',\"";
+
+	inqStr = inqStr + to_string(row.userID) + ",'" + row.userAcc + "','" + row.userPW + "'," + to_string(row.umbrella) + ",'" + row.hashCode + "'";
+	inqStr += "\");";
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniInsert(umbrellas row)
+{
+	string inqStr = "call Uni_insert('umbrella','";
+
+	inqStr = inqStr + to_string(row.umbrellaID) + "," + to_string(row.umbrellaStatus) + "," + to_string(row.locationID) + "," + to_string(row.slotLabel);
+	inqStr += "');";
+
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniInsert(rentalSpot row)
+{
+	string inqStr = "call Uni_insert('rental_spot',\"";
+
+	string umbID, structure, vacancy;
+	for (int i = 0; i < row.umbrellaID.size(); i++)
+	{
+		umbID += to_string(row.umbrellaID[i]);
+		if (i + 1 < row.umbrellaID.size())
+			umbID += ",";
+	}
+	structure = to_string(row.structure.row) + "x" + to_string(row.structure.col);
+	for (int x = 0; x < row.vacancy.size(); x++)
+	{
+		vacancy += row.vacancy[x];
+	}
+	inqStr = inqStr + to_string(row.rsID) + ",'" + umbID + "','" + structure + "','" + vacancy + "'," + to_string(row.lat) + "," + to_string(row.lng);
+	inqStr += "\");";
+
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniDelete(userCol col, string val)
+{
+	string inqStr = "call Uni_delete('user',\"";
+	switch (col)
+	{
+	case user_id:
+		inqStr = inqStr+ "user_id="+val;
+		break;
+	case user_acc:
+		inqStr = inqStr+ "user_acc='"+val+"'";
+		break;
+	case user_pw:
+		inqStr = inqStr+ "user_pw='"+val+"'";
+		break;
+	case umbrella:
+		inqStr += "umbrella="+val;
+		break;
+	case hash_code:
+		inqStr += "hash_code='"+val+"'";
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniDelete(umbrellaCol col, string val)
+{
+	string inqStr = "call Uni_delete('umbrella',\"";
+	switch (col)
+	{
+
+	case umbrella_id:
+		inqStr = inqStr + "umbrella_id=" + val;
+		break;
+	case umbrella_status:
+		inqStr = inqStr+ "umbrella_status="+val;
+		break;
+	case borrower_id:
+		inqStr = inqStr+ "borrower_id="+val;
+		break;
+	case slot_label:
+		inqStr = inqStr+ "slot_label="+val;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniDelete(rentalSpotCol col, string val)
+{
+	string inqStr = "call Uni_delete('rental_spot',\"";
+
+	switch (col)
+	{
+	case rs_id:
+		inqStr = inqStr + "rs_id=" + val;
+		break;
+	case umbrellas_id:
+		inqStr = inqStr + "umbrella_id='" + val + "'";
+		break;
+	case structure:
+		inqStr = inqStr + "structure='" + val + "'";
+		break;
+	case vacancy:
+		inqStr = inqStr + "vacancy='" + val + "'";
+		break;
+	case latitude:
+		inqStr = inqStr + "latitude=" + val;
+		break;
+	case longitude:
+		inqStr = inqStr + "longitude=" + val;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniUpdate(userCol col, string val, userCol conCol, string conVal)
+{
+	string inqStr = "call Uni_update('user',\"";
+	switch (col)
+	{
+	case user_id:
+		inqStr = inqStr + "user_id=" + val;
+		break;
+	case user_acc:
+		inqStr = inqStr + "user_acc='" + val + "'";
+		break;
+	case user_pw:
+		inqStr = inqStr + "user_pw='" + val + "'";
+		break;
+	case umbrella:
+		inqStr += "umbrella=" + val;
+		break;
+	case hash_code:
+		inqStr += "hash_code='" + val + "'";
+		break;
+	default:
+		break;
+	}
+	inqStr += "\",\"";
+	switch (conCol)
+	{
+	case user_id:
+		inqStr = inqStr + "user_id=" + conVal;
+		break;
+	case user_acc:
+		inqStr = inqStr + "user_acc='" + conVal + "'";
+		break;
+	case user_pw:
+		inqStr = inqStr + "user_pw='" + conVal + "'";
+		break;
+	case umbrella:
+		inqStr += "umbrella=" + conVal;
+		break;
+	case hash_code:
+		inqStr += "hash_code='" + conVal + "'";
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniUpdate(umbrellaCol col, string val, umbrellaCol conCol, string conVal)
+{
+	string inqStr = "call Uni_update('umbrella',\"";
+	switch (col)
+	{
+	case umbrella_id:
+		inqStr = inqStr + "umbrella_id=" + val;
+		break;
+	case umbrella_status:
+		inqStr = inqStr + "umbrella_status=" + val;
+		break;
+	case borrower_id:
+		inqStr = inqStr + "borrower_id=" + val;
+		break;
+	case slot_label:
+		inqStr = inqStr + "slot_label=" + val;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\",\"";
+	switch (conCol)
+	{
+	case umbrella_id:
+		inqStr = inqStr + "umbrella_id=" + conVal;
+		break;
+	case umbrella_status:
+		inqStr = inqStr + "umbrella_status=" + conVal;
+		break;
+	case borrower_id:
+		inqStr = inqStr + "borrower_id=" + conVal;
+		break;
+	case slot_label:
+		inqStr = inqStr + "slot_label=" + conVal;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+	return ExeSQL(inqStr);
+}
+
+bool MyDB::UniUpdate(rentalSpotCol col, string val, rentalSpotCol conCol, string conVal)
+{
+	string inqStr = "call Uni_update('rental_spot',\"";
+	switch (col)
+	{
+	case rs_id:
+		inqStr = inqStr + "rs_id=" + val;
+		break;
+	case umbrellas_id:
+		inqStr = inqStr + "umbrella_id='" + val + "'";
+		break;
+	case structure:
+		inqStr = inqStr + "structure='" + val + "'";
+		break;
+	case vacancy:
+		inqStr = inqStr + "vacancy='" + val + "'";
+		break;
+	case latitude:
+		inqStr = inqStr + "latitude=" + val;
+		break;
+	case longitude:
+		inqStr = inqStr + "longitude=" + val;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\",\"";
+	switch (conCol)
+	{
+	case rs_id:
+		inqStr = inqStr + "rs_id=" + conVal;
+		break;
+	case umbrellas_id:
+		inqStr = inqStr + "umbrella_id='" + conVal + "'";
+		break;
+	case structure:
+		inqStr = inqStr + "structure='" + conVal + "'";
+		break;
+	case vacancy:
+		inqStr = inqStr + "vacancy='" + conVal + "'";
+		break;
+	case latitude:
+		inqStr = inqStr + "latitude=" + conVal;
+		break;
+	case longitude:
+		inqStr = inqStr + "longitude=" + conVal;
+		break;
+	default:
+		break;
+	}
+	inqStr += "\");";
+
+	return ExeSQL(inqStr);
+}
+
+//分割字符串
+void MyDB::SplitString(const std::string & s, std::vector<std::string>& v, const std::string & c)
+{
+	std::string::size_type pos1, pos2;
+	pos2 = s.find(c);
+	pos1 = 0;
+	while (std::string::npos != pos2)
+	{
+		v.push_back(s.substr(pos1, pos2 - pos1));
+
+		pos1 = pos2 + c.size();
+		pos2 = s.find(c, pos1);
+	}
+	if (pos1 != s.length())
+		v.push_back(s.substr(pos1));
+}
+
+
 	
 
--- DB/MyDB.h
+++ DB/MyDB.h
@@ -1,11 +1,77 @@
 #include<iostream>  
-#include<string>  
-#include<mysql.h>  
+#include<mysql/mysql.h>  
 #include<vector>
+
 using namespace std;
+
+enum userCol
+{
+	user_id,
+	user_acc,
+	user_pw,
+	umbrella,
+	hash_code
+};
+enum umbrellaCol
+{
+	umbrella_id,
+	umbrella_status,
+	borrower_id,
+	slot_label
+};
+
+enum rentalSpotCol
+{
+	rs_id,
+	umbrellas_id,
+	structure,
+	vacancy,
+	latitude,
+	longitude
+};
+
+struct slotArrangement
+{
+	int row;
+	int col;
+
+};
+struct rentalSpot
+{
+	int rsID;
+	vector<int> umbrellaID;
+	slotArrangement structure;
+	vector<char> vacancy;
+	double lat;
+	double lng;
+};
+struct umbrellas
+{
+	int umbrellaID;
+	int umbrellaStatus;
+	int locationID;
+	int slotLabel;
+};
+struct user
+{
+	int userID;
+	string userAcc;
+	string userPW;
+	int umbrella;
+	string hashCode;
+};
+struct coordinates
+{
+	int lat;
+	int lng;
+};
 
 class MyDB
 {
+private:
+	MYSQL *connection;
+	MYSQL_RES *result;
+	MYSQL_ROW sql_row;
 public:
 	MyDB();
 	~MyDB();
@@ -13,8 +79,21 @@ public:
 	bool ExeSQL(string sql);
 	vector<string> NextRow();
 	void FreeResult();
-private:
-	MYSQL *connection;
-	MYSQL_RES *result;
-	MYSQL_ROW sql_row;
+	void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c);
+	vector<rentalSpot> SearchSpot(double lat, double lng, int range);
+	vector<user> UniSearch(userCol col,string val);
+	vector<umbrellas> UniSearch(umbrellaCol col,string val);
+	vector<rentalSpot> UniSearch(rentalSpotCol col, string val);
+
+	bool UniInsert(user row);
+	bool UniInsert(umbrellas row);
+	bool UniInsert(rentalSpot row);
+
+	bool UniDelete(userCol col, string val);
+	bool UniDelete(umbrellaCol col, string val);
+	bool UniDelete(rentalSpotCol col, string val);
+
+	bool UniUpdate(userCol col, string val, userCol conCol, string conVal);
+	bool UniUpdate(umbrellaCol col, string val, umbrellaCol conCol, string conVal);
+	bool UniUpdate(rentalSpotCol col, string val, rentalSpotCol conCol, string conVal);
 };
